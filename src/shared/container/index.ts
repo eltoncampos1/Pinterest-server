03d3aa0c968b6ce972da395ef22fd82faf63ad79
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-22 18:40:40 -0300

    chore: register tokenprovider instance

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,6 +3,8 @@ import { BCryptHashProvider } from '../../modules/users/providers/hash-provider/
 import { IHashProvider } from '../../modules/users/providers/hash-provider/models/IHashProvider';
 import { IUsersRepository } from '../../modules/users/repositories/IUsersRepositories';
 import { PostgresUSersRepository } from '../../modules/users/repositories/implementations/PostgresUsersRepository';
+import { ITokenProvider, JsonWebTokenProvider } from '../../modules/users/providers';
 
 container.registerSingleton<IUsersRepository>('UsersRepository', PostgresUSersRepository);
 container.registerSingleton<IHashProvider>('HashProvider', BCryptHashProvider);
+container.registerSingleton<ITokenProvider>('TokenProvider', JsonWebTokenProvider);
