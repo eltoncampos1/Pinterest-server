e529f422c1bdee5c107552a1d8f4fd42b1f03450
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-22 11:33:53 -0300

    faet: cretae user controller

---
+++ src/modules/users/use-cases/create-user-controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { CreateUserUseCase } from './create-user';
+
+export class CreateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { email, password, age } = request.body;
+
+    const createUserUseCase = container.resolve(CreateUserUseCase);
+
+    const user = await createUserUseCase.execute({ email, password, age });
+
+    return response.status(201).json(user);
+  }
+}
