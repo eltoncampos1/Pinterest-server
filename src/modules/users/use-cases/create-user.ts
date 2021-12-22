62f429fc6c1ae5ab4473cba9dc987207a08a6eed
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-22 11:28:28 -0300

    feat: create user use case

---
+++ src/modules/users/use-cases/create-user.ts
@@ -0,0 +1,31 @@
+/* eslint-disable prettier/prettier */
+import { inject, injectable } from 'tsyringe';
+import { AppError } from '../../../shared/errors/errors';
+import { ICreateUserDTO } from '../dtos';
+import { User } from '../entities/User';
+import { IHashProvider } from '../providers/hash-provider/models/IHashProvider';
+import { IUsersRepository } from '../repositories/IUsersRepositories';
+
+@injectable()
+export class CreateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    private hashProvider: IHashProvider
+  ) { }
+
+  async execute({ email, password, age }: ICreateUserDTO): Promise<User | undefined> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email)
+
+    if (userAlreadyExists) {
+      throw new AppError('Email already registered in our system')
+    }
+
+    const hashedPassword = await this.hashProvider.generateHash(password)
+
+    const user = await this.usersRepository.create({ email, password: hashedPassword, age });
+
+    return user;
+  }
+}
